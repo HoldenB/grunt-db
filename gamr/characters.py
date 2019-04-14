3c5688c898f3ea5c5030f15da94580555f2a9e33
Holden.B <holden.bab@outlook.com>
2019-04-13 23:20:18 -0500

    Added create/update/delete character options

--- gamr/characters.py
+++ gamr/characters.py
@@ -15,9 +15,9 @@ def index():
     db = get_db()
     # Show all characters created by the given user
     characters = db.execute(
-        'SELECT c.id, e_wep_id, user_id, c.created, hours_played, character_name, character_level, character_kills, u.username, e.weapon_name'
-        ' FROM character c JOIN user u JOIN equipped_weapon e'
-        ' ON c.user_id = u.id AND c.e_wep_id = e.id'
+        'SELECT c.id, e_wep_id, user_id, c.created, hours_played, character_name, character_level, character_kills, u.username, e.weapon_name, rarity, zone_name'
+        ' FROM character c JOIN user u JOIN equipped_weapon e JOIN zone z'
+        ' ON c.user_id = u.id AND c.e_wep_id = e.id AND e.zone_id = z.id'
         ' ORDER BY c.created DESC'
     ).fetchall()
 
@@ -28,24 +28,22 @@ def index():
 @login_required
 def create():
     if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
+        character_name = request.form['character_name']
         error = None
 
-        if not title:
-            error = 'Title is required.'
-
-        if not body:
-            error = "Message body is required."
+        if not character_name:
+            error = 'Character name is required.'
 
         if error is not None:
             flash(error)
         else:
             db = get_db()
+            # When a new character is created, we will default
+            # the character to the starting zone with a starter weapon
             db.execute(
-                'INSERT INTO post (title, body, author_id)'
-                ' VALUES (?, ?, ?)',
-                (title, body, g.user['id'])
+                'INSERT INTO character (e_wep_id, user_id, hours_played, character_name, character_level, character_kills)'
+                ' VALUES (?, ?, ?, ?, ?, ?)',
+                (1, g.user['id'], 0, character_name, 1, 0)
             )
             db.commit()
 
@@ -54,56 +52,55 @@ def create():
     return render_template('characters/create.html')
 
 
-def get_post(id, check_author=True):
-    post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' WHERE p.id = ?',
+def get_character(id, check_user=True):
+    character = get_db().execute(
+        'SELECT c.id, e_wep_id, user_id, created, hours_played, character_name, character_level, character_kills'
+        ' FROM character c JOIN user u ON c.user_id = u.id'
+        ' WHERE c.id = ?',
         (id,)
     ).fetchone()
 
-    if post is None:
-        abort(404, "Post id {0} doesn't exist.".format(id))
+    if character is None:
+        abort(404, "Character id {0} doesn't exist.".format(id))
 
-    if check_author and post['author_id'] != g.user['id']:
+    if check_user and character['user_id'] != g.user['id']:
         abort(403)
 
-    return post
+    return character
 
 
 @bp.route('/<int:id>/update', methods=('GET', 'POST'))
 @login_required
 def update(id):
-    post = get_post(id)
+    character = get_character(id)
 
     if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
+        character_name = request.form['character_name']
         error = None
 
-        if not title:
-            error = 'Title is required.'
+        if not character_name:
+            error = 'Character name is required.'
 
         if error is not None:
             flash(error)
         else:
             db = get_db()
             db.execute(
-                'UPDATE post SET title = ?, body = ?'
+                'UPDATE character SET character_name = ?'
                 ' WHERE id = ?',
-                (title, body, id)
+                (character_name, id)
             )
             db.commit()
             return redirect(url_for('characters.index'))
 
-    return render_template('characters/update.html', post=post)
+    return render_template('characters/update.html', character=character)
 
 
 @bp.route('/<int:id>/delete', methods=('POST',))
 @login_required
 def delete(id):
-    get_post(id)
+    get_character(id)
     db = get_db()
-    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.execute('DELETE FROM character WHERE id = ?', (id,))
     db.commit()
     return redirect(url_for('characters.index'))
--- gamr/game_db_schema.sql
+++ gamr/game_db_schema.sql
@@ -3,12 +3,14 @@ DROP TABLE IF EXISTS zone;
 DROP TABLE IF EXISTS equipped_weapon;
 DROP TABLE IF EXISTS character;
 
+-- User table
 CREATE TABLE user (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL
 );
 
+-- Zone table
 CREATE TABLE zone (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -19,6 +21,7 @@ CREATE TABLE zone (
     difficulty TEXT NOT NULL
 );
 
+-- User "Equipped Weapon Slot" table
 CREATE TABLE equipped_weapon (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -30,6 +33,7 @@ CREATE TABLE equipped_weapon (
     FOREIGN KEY (zone_id) REFERENCES zone (id)
 );
 
+-- Character table
 CREATE TABLE character (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     e_wep_id INTEGER NOT NULL,
@@ -43,14 +47,56 @@ CREATE TABLE character (
     FOREIGN KEY (user_id) REFERENCES user (id)
 );
 
-INSERT INTO user (username, password)
-VALUES ('Prateek', 'bbw');
+------------------------------------------------------------------------------------------
+-- ZONES
+-- Premade zone 1
+INSERT INTO zone (num_characters, num_creatures, zone_name, zone_level_range, difficulty)
+VALUES ('300', '300', 'Elwynn Forest', '1-20', 'Easy');
 
-INSERT INTO character (e_wep_id, user_id, hours_played, character_name, character_level, character_kills)
-VALUES ('1', '1', '10', 'bbwLover', '100', '200');
-
-INSERT INTO equipped_weapon (zone_id, weapon_name, weapon_level, weapon_type, rarity)
-VALUES ('1', 'Prateek''s Mighty Hammer', '100', 'Hammer', 'Epic');
-
+-- Premade zone 2
 INSERT INTO zone (num_characters, num_creatures, zone_name, zone_level_range, difficulty)
 VALUES ('100', '100', 'Prateek''s Valley', '90-100', 'Very Hard');
+
+-- Premade zone 3
+INSERT INTO zone (num_characters, num_creatures, zone_name, zone_level_range, difficulty)
+VALUES ('40', '250', 'The Green Hills', '50-60', 'Moderately Hard');
+
+------------------------------------------------------------------------------------------
+-- WEAPONS
+-- Premade wep 1
+INSERT INTO equipped_weapon (zone_id, weapon_name, weapon_level, weapon_type, rarity)
+VALUES ('1', 'Crude sword', '1', 'One-Hand Sword', 'Common');
+
+-- Premade wep 2
+INSERT INTO equipped_weapon (zone_id, weapon_name, weapon_level, weapon_type, rarity)
+VALUES ('2', 'Prateek''s Mighty Hammer', '100', 'Hammer', 'Epic');
+
+-- Premade wep 3
+INSERT INTO equipped_weapon (zone_id, weapon_name, weapon_level, weapon_type, rarity)
+VALUES ('3', ' Dank-A-Lank MayMays', '80', 'Sword', 'Legendary');
+
+-- Premade wep 4
+INSERT INTO equipped_weapon (zone_id, weapon_name, weapon_level, weapon_type, rarity)
+VALUES ('3', 'Finger Gun', '20', 'Gun', 'Common');
+
+------------------------------------------------------------------------------------------
+-- Demo user 1
+INSERT INTO user (username, password)
+VALUES ('Jack', 'Jack');
+-- Demo character 1
+INSERT INTO character (e_wep_id, user_id, hours_played, character_name, character_level, character_kills)
+VALUES ('2', '1', '10', 'BWWorBBW', '100', '200');
+
+-- Demo user 2
+INSERT INTO user (username, password)
+VALUES ('SBAV', 'SBAV');
+-- Demo character 2
+INSERT INTO character (e_wep_id, user_id, hours_played, character_name, character_level, character_kills)
+VALUES ('3', '2', '60', 'AKTheMemeLord', '80', '1000');
+
+-- Demo user 3
+INSERT INTO user (username, password)
+VALUES ('T8', 'T8');
+-- Demo character 3
+INSERT INTO character (e_wep_id, user_id, hours_played, character_name, character_level, character_kills)
+VALUES ('4', '3', '90', 'BarryMcKokinner', '90', '850');
